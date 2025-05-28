f15c21617c3fe05f2cb958593f3cab4030cd1c14
virennasystems <rosen_notizbuch0e@icloud.com>
2025-05-28 17:34:41 +0200

    Update service-worker.js new

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,21 @@
-const CACHE_NAME = 'virenna-cache-v2';
+const CACHE_NAME = 'virenna-cache-v1';
 const FILES_TO_CACHE = [
   './',
   './index.html',
+  './offline.html',
   './deckblatt.PNG',
   './van.PNG',
   './himalaya.PNG',
+  './pattern.svg',
+  './VIRENNA_Siegel_Transparent.png',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png',
-  './icon-144.png',
-  './icon-256.png',
-  './offline.html',
-  './VIRENNA_Siegel_Transparent.png',
-  './pattern.svg'
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (evt) => {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
@@ -27,26 +23,20 @@ self.addEventListener('install', (evt) => {
 self.addEventListener('activate', (evt) => {
   evt.waitUntil(
     caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
-  if (evt.request.method !== 'GET') return;
-
   evt.respondWith(
     caches.match(evt.request).then((response) => {
-      return response || fetch(evt.request).catch(() => {
-        return caches.match('./offline.html');
-      });
+      return response || fetch(evt.request).catch(() => caches.match('./offline.html'));
     })
   );
 });
